abacd89bd04d9a3081da62971cdc02a858f81465
Hernandez Sanchez Juan Carlos <juanc.hernandezs06@gmail.com>
2024-10-17 10:46:26 -0600

    Holal ej 6 a 9

--- arreglos8.c
+++ arreglos8.c
@@ -6,19 +6,47 @@
 //  @return varible de tipo int
 //  @param una matriz
 //  @param el número de filas
-
+int suma(int a[][3], int f);
+int suma2(int **a, int f, int c);
 
 int main() {
     int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-    // TODO: (23) Crea una variable llamada resultado  y asigna lo que devuelve la función suma
-    // pasa a la función el arreglo y su tamaño
+
+    int res= suma(arr, 3);
+    int *ptr = &res;
+    //res= suma2(arr, 3, 3);
     
-    
-    // TODO: (24) Imprime el resultado
-    
+            printf("%d\t", *(ptr)); 
+          //printf("%d\t", *(ptr)); 
+        
     return 0;
+}
+
+int suma2(int **a, int f, int c){
+    int r= 0;
+    for (int i = 0; i < f; i++) {
+        for (int j = 0; j < c; j++) {
+            r += a[i][j]; //PTR guarda la direccion de memoria del primer elemento
+            //TODO: (21) Usa la lógica de apuntadores para acceder al arreglo
+        }
+        
+    }
+    return r;
+
+
 }
 
 
 //TODO: (25) Crea la función suma que realiza la suma de todos los elementos de un arreglo
+int suma(int a[][3], int f){
+    int r= 0;
+    for (int i = 0; i < f; i++) {
+        for (int j = 0; j < 3; j++) {
+            r += a[i][j]; //PTR guarda la direccion de memoria del primer elemento
+            //TODO: (21) Usa la lógica de apuntadores para acceder al arreglo
+        }
+        
+    }
+    return r;
+}
